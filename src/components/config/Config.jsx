20b0965939f9b8d2340979d2aca1df658065cc88
0xRomRom <roman0811@live.nl>
2024-11-30 03:28:11 +0100

    All tabs added

--- src/components/config/Config.jsx
+++ src/components/config/Config.jsx
@@ -343,218 +343,230 @@ const Config = ({
               )}
             </div>
           )}
-
-          <div
-            className={`${stl.box} ${activeTab === 3 ? stl.activeBg : ""}`}
-            onClick={() => setActiveTab(3)}
-          >
-            <div className={stl.topRow}>
-              <span className={stl.title}>
-                <FaRulerVertical className={stl.icon} />
-                Lengte
-              </span>
-            </div>
-
-            {activeTab === 3 && (
-              <div className={stl.lengthBox}>
-                <input
-                  type="range"
-                  min="40"
-                  max="240"
-                  step="10"
-                  value={customLength}
-                  onChange={(e) => setCustomLength(e.target.value)}
-                  className={stl.rangeInput}
-                />
-                <span className={stl.rangeValue}>{customLength}CM</span>
+          {((window.innerWidth < 500 && activeTab === 3) ||
+            window.innerWidth > 500) && (
+            <div
+              className={`${stl.box} ${activeTab === 3 ? stl.activeBg : ""}`}
+              onClick={() => setActiveTab(3)}
+            >
+              <div className={stl.topRow}>
+                <span className={stl.title}>
+                  <FaRulerVertical className={stl.icon} />
+                  Lengte
+                </span>
               </div>
-            )}
-          </div>
 
-          <div
-            className={`${stl.box} ${activeTab === 4 ? stl.activeBg : ""}`}
-            onClick={() => setActiveTab(4)}
-          >
-            <div className={stl.topRow}>
-              <span className={stl.title}>
-                <IoColorPaletteOutline className={stl.icon} />
-                Achterpaneel Kleur
-              </span>
-              {activeTab === 4 && (
-                <button onClick={() => setVideoOverlay(videoIDs[0])}>
-                  <FiYoutube />
-                  Voorbeeld
-                </button>
-              )}
-            </div>
-
-            {activeTab === 4 && (
-              <div className={stl.choiceCtas}>
-                <button
-                  className={
-                    backPanelColor === "Transparant" ? stl.activePlateCta : ""
-                  }
-                  onClick={() => setBackPanelColor("Transparant")}
-                >
-                  Transparant
-                </button>
-                <button
-                  className={
-                    backPanelColor === "Zwart" ? stl.activePlateCta : ""
-                  }
-                  onClick={() => setBackPanelColor("Zwart")}
-                >
-                  Zwart (+25%)
-                </button>
-              </div>
-            )}
-          </div>
-
-          <div
-            className={`${stl.box} ${activeTab === 5 ? stl.activeBg : ""}`}
-            onClick={() => setActiveTab(5)}
-          >
-            <div className={stl.topRow}>
-              <span className={stl.title}>
-                <RiShape2Fill className={stl.icon} />
-                Achterpaneel Vorm
-              </span>
-              {activeTab === 5 && (
-                <button onClick={() => setVideoOverlay(videoIDs[1])}>
-                  <FiYoutube />
-                  Voorbeeld
-                </button>
-              )}
-            </div>
-
-            {activeTab === 5 && (
-              <div className={stl.choiceCtas}>
-                <button
-                  className={
-                    backPlateShape === "Rechthoekig" ? stl.activePlateCta : ""
-                  }
-                  onClick={() => setBackPlateShape("Rechthoekig")}
-                >
-                  Rechthoekig
-                </button>
-                <button
-                  className={
-                    backPlateShape === "Contour" ? stl.activePlateCta : ""
-                  }
-                  onClick={() => setBackPlateShape("Contour")}
-                >
-                  Contour
-                </button>
-              </div>
-            )}
-          </div>
-
-          <div
-            className={`${stl.box} ${activeTab === 6 ? stl.activeBg : ""}`}
-            onClick={() => setActiveTab(6)}
-          >
-            <div className={stl.topRow}>
-              <span className={stl.title}>
-                <IoColorPaletteOutline className={stl.icon} />
-                Voorbeeld Achtergrond
-              </span>
-            </div>
-            {activeTab === 6 && (
-              <div className={stl.content}>
-                <div className={stl.bgGrid}>
-                  {backgrounds.map((background, index) => (
-                    <div
-                      key={index}
-                      onClick={() => {
-                        setBackgroundImage(background);
-                        setCustomBg("");
-                        if (index === backgrounds.length - 1) {
-                          uploadImage();
-                        }
-                      }}
-                      className={`${stl.bgDiv} ${
-                        backgroundImage === background ? stl.selectedBg : ""
-                      }`}
-                      style={{
-                        backgroundImage: `url(${
-                          index !== backgrounds.length - 1
-                            ? background
-                            : customBg
-                        })`,
-                        border:
-                          index === backgrounds.length - 1
-                            ? "1px solid rgb(134, 0, 134)"
-                            : "",
-                      }}
-                    >
-                      {index === backgrounds.length - 1 && (
-                        <span>Upload Foto</span>
-                      )}
-                    </div>
-                  ))}
+              {activeTab === 3 && (
+                <div className={stl.lengthBox}>
+                  <input
+                    type="range"
+                    min="40"
+                    max="240"
+                    step="10"
+                    value={customLength}
+                    onChange={(e) => setCustomLength(e.target.value)}
+                    className={stl.rangeInput}
+                  />
+                  <span className={stl.rangeValue}>{customLength}CM</span>
                 </div>
-
-                <input
-                  type="file"
-                  accept="image/*"
-                  ref={fileInputRef}
-                  style={{ display: "none" }}
-                  onChange={handleFileChange}
-                />
-              </div>
-            )}
-          </div>
-
-          <div
-            className={`${stl.box} ${activeTab === 7 ? stl.activeBg : ""}`}
-            onClick={() => setActiveTab(7)}
-          >
-            <div className={stl.topRow}>
-              <span className={stl.title}>
-                <GiScrew className={stl.icon} />
-                Montage Methode
-              </span>
-              {activeTab === 7 && (
-                <button onClick={() => setVideoOverlay(videoIDs[2])}>
-                  <FiYoutube />
-                  Voorbeeld
-                </button>
               )}
             </div>
-
-            {activeTab === 7 && (
-              <div className={stl.choiceCtas}>
-                <button
-                  className={
-                    mountingMethod === "Afstandhouders"
-                      ? stl.activePlateCta
-                      : ""
-                  }
-                  onClick={() => setMountingMethod("Afstandhouders")}
-                >
-                  Afstandhouders
-                </button>
-                <button
-                  className={
-                    mountingMethod === "Ketting" ? stl.activePlateCta : ""
-                  }
-                  onClick={() => setMountingMethod("Ketting")}
-                >
-                  Ketting
-                </button>
-                {backPanelColor === "Zwart" && (
-                  <button
-                    className={
-                      mountingMethod === "Railmontage" ? stl.activePlateCta : ""
-                    }
-                    onClick={() => setMountingMethod("Railmontage")}
-                  >
-                    Railmontage
+          )}
+          {((window.innerWidth < 500 && activeTab === 4) ||
+            window.innerWidth > 500) && (
+            <div
+              className={`${stl.box} ${activeTab === 4 ? stl.activeBg : ""}`}
+              onClick={() => setActiveTab(4)}
+            >
+              <div className={stl.topRow}>
+                <span className={stl.title}>
+                  <IoColorPaletteOutline className={stl.icon} />
+                  Achterpaneel Kleur
+                </span>
+                {activeTab === 4 && (
+                  <button onClick={() => setVideoOverlay(videoIDs[0])}>
+                    <FiYoutube />
+                    Voorbeeld
                   </button>
                 )}
               </div>
-            )}
-          </div>
+
+              {activeTab === 4 && (
+                <div className={stl.choiceCtas}>
+                  <button
+                    className={
+                      backPanelColor === "Transparant" ? stl.activePlateCta : ""
+                    }
+                    onClick={() => setBackPanelColor("Transparant")}
+                  >
+                    Transparant
+                  </button>
+                  <button
+                    className={
+                      backPanelColor === "Zwart" ? stl.activePlateCta : ""
+                    }
+                    onClick={() => setBackPanelColor("Zwart")}
+                  >
+                    Zwart (+25%)
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
+          {((window.innerWidth < 500 && activeTab === 5) ||
+            window.innerWidth > 500) && (
+            <div
+              className={`${stl.box} ${activeTab === 5 ? stl.activeBg : ""}`}
+              onClick={() => setActiveTab(5)}
+            >
+              <div className={stl.topRow}>
+                <span className={stl.title}>
+                  <RiShape2Fill className={stl.icon} />
+                  Achterpaneel Vorm
+                </span>
+                {activeTab === 5 && (
+                  <button onClick={() => setVideoOverlay(videoIDs[1])}>
+                    <FiYoutube />
+                    Voorbeeld
+                  </button>
+                )}
+              </div>
+
+              {activeTab === 5 && (
+                <div className={stl.choiceCtas}>
+                  <button
+                    className={
+                      backPlateShape === "Rechthoekig" ? stl.activePlateCta : ""
+                    }
+                    onClick={() => setBackPlateShape("Rechthoekig")}
+                  >
+                    Rechthoekig
+                  </button>
+                  <button
+                    className={
+                      backPlateShape === "Contour" ? stl.activePlateCta : ""
+                    }
+                    onClick={() => setBackPlateShape("Contour")}
+                  >
+                    Contour
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
+          {((window.innerWidth < 500 && activeTab === 6) ||
+            window.innerWidth > 500) && (
+            <div
+              className={`${stl.box} ${activeTab === 6 ? stl.activeBg : ""}`}
+              onClick={() => setActiveTab(6)}
+            >
+              <div className={stl.topRow}>
+                <span className={stl.title}>
+                  <IoColorPaletteOutline className={stl.icon} />
+                  Voorbeeld Achtergrond
+                </span>
+              </div>
+              {activeTab === 6 && (
+                <div className={stl.content}>
+                  <div className={stl.bgGrid}>
+                    {backgrounds.map((background, index) => (
+                      <div
+                        key={index}
+                        onClick={() => {
+                          setBackgroundImage(background);
+                          setCustomBg("");
+                          if (index === backgrounds.length - 1) {
+                            uploadImage();
+                          }
+                        }}
+                        className={`${stl.bgDiv} ${
+                          backgroundImage === background ? stl.selectedBg : ""
+                        }`}
+                        style={{
+                          backgroundImage: `url(${
+                            index !== backgrounds.length - 1
+                              ? background
+                              : customBg
+                          })`,
+                          border:
+                            index === backgrounds.length - 1
+                              ? "1px solid rgb(134, 0, 134)"
+                              : "",
+                        }}
+                      >
+                        {index === backgrounds.length - 1 && (
+                          <span>Upload Foto</span>
+                        )}
+                      </div>
+                    ))}
+                  </div>
+
+                  <input
+                    type="file"
+                    accept="image/*"
+                    ref={fileInputRef}
+                    style={{ display: "none" }}
+                    onChange={handleFileChange}
+                  />
+                </div>
+              )}
+            </div>
+          )}
+          {((window.innerWidth < 500 && activeTab === 7) ||
+            window.innerWidth > 500) && (
+            <div
+              className={`${stl.box} ${activeTab === 7 ? stl.activeBg : ""}`}
+              onClick={() => setActiveTab(7)}
+            >
+              <div className={stl.topRow}>
+                <span className={stl.title}>
+                  <GiScrew className={stl.icon} />
+                  Montage Methode
+                </span>
+                {activeTab === 7 && (
+                  <button onClick={() => setVideoOverlay(videoIDs[2])}>
+                    <FiYoutube />
+                    Voorbeeld
+                  </button>
+                )}
+              </div>
+
+              {activeTab === 7 && (
+                <div className={stl.choiceCtas}>
+                  <button
+                    className={
+                      mountingMethod === "Afstandhouders"
+                        ? stl.activePlateCta
+                        : ""
+                    }
+                    onClick={() => setMountingMethod("Afstandhouders")}
+                  >
+                    Afstandhouders
+                  </button>
+                  <button
+                    className={
+                      mountingMethod === "Ketting" ? stl.activePlateCta : ""
+                    }
+                    onClick={() => setMountingMethod("Ketting")}
+                  >
+                    Ketting
+                  </button>
+                  {backPanelColor === "Zwart" && (
+                    <button
+                      className={
+                        mountingMethod === "Railmontage"
+                          ? stl.activePlateCta
+                          : ""
+                      }
+                      onClick={() => setMountingMethod("Railmontage")}
+                    >
+                      Railmontage
+                    </button>
+                  )}
+                </div>
+              )}
+            </div>
+          )}
         </div>
       )}
       {activeTab === 8 && (
@@ -563,47 +575,50 @@ const Config = ({
           <button>Terug naar instellingen</button>
         </div>
       )}
-      <div
-        className={`${stl.box} ${activeTab === 8 ? stl.activeBg : ""}`}
-        onClick={() => setActiveTab(8)}
-      >
-        <div className={stl.topRow}>
-          <span
-            className={stl.title}
-            style={{
-              cursor: activeTab === 8 ? "initial" : "pointer",
-            }}
-          >
-            <FaCreditCard className={stl.icon} />
-            Afrekenen
-          </span>
+      {((window.innerWidth < 500 && activeTab === 8) ||
+        window.innerWidth > 500) && (
+        <div
+          className={`${stl.box} ${activeTab === 8 ? stl.activeBg : ""}`}
+          onClick={() => setActiveTab(8)}
+        >
+          <div className={stl.topRow}>
+            <span
+              className={stl.title}
+              style={{
+                cursor: activeTab === 8 ? "initial" : "pointer",
+              }}
+            >
+              <FaCreditCard className={stl.icon} />
+              Afrekenen
+            </span>
+            {activeTab === 8 && (
+              <button onClick={() => setVideoOverlay(videoIDs[3])}>
+                <FiYoutube />
+                Voorbeeld
+              </button>
+            )}
+          </div>
+
           {activeTab === 8 && (
-            <button onClick={() => setVideoOverlay(videoIDs[3])}>
-              <FiYoutube />
-              Voorbeeld
-            </button>
+            <div className={stl.checkoutDiv}>
+              <textarea
+                placeholder="Opmerkingen (optioneel)"
+                value={checkoutMessage}
+                onInput={(e) => setCheckoutMessage(e.target.value)}
+              ></textarea>
+              <button
+                disabled={checkoutLoading ? true : false}
+                onClick={submitForm}
+              >
+                {checkoutLoading && (
+                  <img src="./images/Spinner.svg" alt="Spinner" />
+                )}
+                {!checkoutLoading && "Afrekenen"}
+              </button>
+            </div>
           )}
         </div>
-
-        {activeTab === 8 && (
-          <div className={stl.checkoutDiv}>
-            <textarea
-              placeholder="Opmerkingen (optioneel)"
-              value={checkoutMessage}
-              onInput={(e) => setCheckoutMessage(e.target.value)}
-            ></textarea>
-            <button
-              disabled={checkoutLoading ? true : false}
-              onClick={submitForm}
-            >
-              {checkoutLoading && (
-                <img src="./images/Spinner.svg" alt="Spinner" />
-              )}
-              {!checkoutLoading && "Afrekenen"}
-            </button>
-          </div>
-        )}
-      </div>
+      )}
     </div>
   );
 };
